793435a4928bdcabe9c992145d1dea9f9c7ba7a0
unaxgomez <unaxhp@hotmail.com>
2022-01-18 22:13:36 +0100

    mensaje

---
+++ Prueba/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prueba
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("me cago en mi puta madre");
+        }
+    }
+}
